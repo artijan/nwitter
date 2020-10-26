645c2b539b05a47d2ea9afc95787b7a90b25580f
artijan <hunbari123@gmail.com>
2020-10-27 08:44:54 +0900

    Updates

---
+++ precache-manifest.7f3275f9030f4f546fa84a5f44b11eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38bd7ffc2509d72b488a73b1c3c03bd",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "d91008b177cde2143486",
+    "url": "/nwitter/static/css/main.5a5e0eeb.chunk.css"
+  },
+  {
+    "revision": "7725448536165184ba33",
+    "url": "/nwitter/static/js/2.6404da7b.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/nwitter/static/js/2.6404da7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91008b177cde2143486",
+    "url": "/nwitter/static/js/main.46e16e37.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
